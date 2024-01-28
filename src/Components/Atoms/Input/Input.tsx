a547f62b710982ea7be7858e89d3959dea9e5e3c
leoFagundes <leofagundes2015@gmail.com>
2024-01-28 15:55:28 -0300

    update molecules

--- src/Components/Atoms/Input/Input.tsx
+++ src/Components/Atoms/Input/Input.tsx
@@ -1,34 +1,37 @@
 import styles from './Input.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, ReactNode, useState } from "react";
 
 type InputType = {
-    value: string;
-    placeholder: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    errorLabel?: string;
-    type?: string;
+  value: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  errorLabel?: string;
+  type?: string;
+  marginTop?: string;
+  caption?: ReactNode;
 }
-export const Input = ({ value, placeholder, onChange, errorLabel, type }: InputType) => {
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+export const Input = ({ value, placeholder, onChange, errorLabel, type, marginTop, caption }: InputType) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
 
-    const handlePassword = () => {
-        setIsPasswordVisible(!isPasswordVisible)
-    }
+  const handlePassword = () => {
+    setIsPasswordVisible(!isPasswordVisible)
+  }
 
-    const IS_ERROR_INPUT_STYLE = errorLabel ? styles.inputContainer__isNegative : styles.inputContainer;
-    const IS_PASSWORD_VISIBLE_ICON = isPasswordVisible ? faEye : faEyeSlash;
-    const IS_PASSWORD_VISIBLE_TYPE = isPasswordVisible ? undefined : type;
-    return (
-        <div className={styles.container}>
-            <input type={IS_PASSWORD_VISIBLE_TYPE} value={value} placeholder={placeholder} onChange={onChange} className={IS_ERROR_INPUT_STYLE} />
-            {type === 'password' &&
-                <div aria-label='icone de olho' onClick={handlePassword} className={styles.icon}>
-                    <FontAwesomeIcon color='#4a4a4a' size='lg' icon={IS_PASSWORD_VISIBLE_ICON} />
-                </div>
-            }
-            {errorLabel && <label className={styles.labelError}>{errorLabel}</label>}
+  const IS_ERROR_INPUT_STYLE = errorLabel ? styles.inputContainer__isNegative : styles.inputContainer;
+  const IS_PASSWORD_VISIBLE_ICON = isPasswordVisible ? faEye : faEyeSlash;
+  const IS_PASSWORD_VISIBLE_TYPE = isPasswordVisible ? undefined : type;
+  return (
+    <div style={{ marginTop: marginTop }} className={styles.container}>
+      <input type={IS_PASSWORD_VISIBLE_TYPE} value={value} placeholder={placeholder} onChange={onChange} className={IS_ERROR_INPUT_STYLE} />
+      {type === 'password' &&
+        <div aria-label='icone de olho' onClick={handlePassword} className={styles.icon}>
+          <FontAwesomeIcon color='#4a4a4a' size='lg' icon={IS_PASSWORD_VISIBLE_ICON} />
         </div>
-    )
+      }
+      {errorLabel && <label className={styles.labelError}>{errorLabel}</label>}
+      {caption && <label className={styles.caption}>{caption}</label>}
+    </div>
+  )
 }
--- src/Components/Atoms/Text/index.tsx
+++ src/Components/Atoms/Text/index.tsx
@@ -1,25 +1,33 @@
 import style from './Text.module.scss'
 
 type Props = {
-    children: string
-    isLink?: boolean
-    fontSize?: "small" | "mediumSmall" | "medium" | "mediumLarge" | "large" | "extraLarge";
+  children: string
+  isLink?: boolean
+  fontSize?: "small" | "mediumSmall" | "medium" | "mediumLarge" | "large" | "extraLarge";
+  fontWeight?: "regular" | "medium" | "semibold" | "bold";
 }
 
-export default function Text({ children, isLink = false, fontSize = "medium" }: Props) {
+export default function Text({ children, isLink = false, fontSize = "medium", fontWeight = "regular" }: Props) {
 
-    const fontSizeClass = {
-        small: style.fontSizeSmall,
-        mediumSmall: style.fontSizeMediumSmall,
-        medium: style.fontSizeMedium,
-        mediumLarge: style.fontSizeMediumLarge,
-        large: style.fontSizeLarge,
-        extraLarge: style.fontSizeExtraLarge,
-    }[fontSize];
+  const fontSizeClass = {
+    small: style.fontSizeSmall,
+    mediumSmall: style.fontSizeMediumSmall,
+    medium: style.fontSizeMedium,
+    mediumLarge: style.fontSizeMediumLarge,
+    large: style.fontSizeLarge,
+    extraLarge: style.fontSizeExtraLarge,
+  }[fontSize];
 
-    return (
-        <p className={`${style.mainText} ${isLink ? style.isLink : ''} ${fontSizeClass}`}>
-            {children}
-        </p>
-    )
+  const fontWeightClass = {
+    regular: style.fontWeightRegular,
+    medium: style.fontWeightMedium,
+    semibold: style.fontWeightSemibold,
+    bold: style.fontWeightBold
+  }[fontWeight]
+
+  return (
+    <p className={`${style.mainText} ${isLink ? style.isLink : ''} ${fontSizeClass} ${fontWeightClass}`}>
+      {children}
+    </p>
+  )
 }

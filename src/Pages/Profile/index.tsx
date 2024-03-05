4766f1473e4a44f91e3af6696e4db3cfc01d6646
leoFagundes <leofagundes2015@gmail.com>
2024-03-05 14:29:14 -0300

    feat: update details

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -5,11 +5,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faUserCheck, faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { profileIconData } from "./profileIconData";
 import UserRepositories from '../../Services/repositories/UserRepositories';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Profile() {
-  const [imageLoaded, setImageLoaded] = useState(false);
+  const [loadingIndex, setLoadingIndex] = useState(0);
   const { user, updateUser } = useAuth();
+  const requestInterval = 1;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (loadingIndex < profileIconData.length) {
+        setLoadingIndex(prevIndex => prevIndex + 1);
+      }
+    }, requestInterval);
+    return () => clearInterval(interval);
+  }, [loadingIndex]);
 
   const handleClick = async (profileImageUpdate: string) => {
     let updatedProfileImage = profileImageUpdate;
@@ -77,28 +87,20 @@ export default function Profile() {
           <div onClick={() => handleClick('random')} className={`${styles.userDefaultProfileImage}`}>
             <FontAwesomeIcon size="2xl" icon={faQuestion} />
           </div>
-          {profileIconData.map(({ seed }, index) => (
+          {profileIconData.slice(0, loadingIndex).map(({ seed }, index) => (
             <div key={index}>
-              {!imageLoaded &&
-                <div className={styles.loading}>
-                  <span className={styles.loader}></span>
-                </div>
-              }
               <img
                 className={`${styles.profileIcon}`}
                 src={`https://api.dicebear.com/7.x/adventurer/svg?seed=${seed}`}
                 alt={"avatar" + seed}
                 onClick={() => handleClick(seed)}
-                onLoad={() => setImageLoaded(true)}
               />
             </div>
           ))}
         </div>
-
       </section>
     )
-  }
-  else {
+  } else {
     return <></>
   }
 }

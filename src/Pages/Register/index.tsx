48e585048d85b5d86150dd9fc2bca8fb7ee6265a
leoFagundes <leofagundes2015@gmail.com>
2024-03-08 07:16:05 -0300

    update error in register items

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -1,77 +1,81 @@
-import Text from '../../Components/Atoms/Text'
-import styles from './Register.module.scss'
+import Text from "../../Components/Atoms/Text";
+import styles from "./Register.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBurger, faWineBottle } from "@fortawesome/free-solid-svg-icons";
-import RegisterCard from '../../Components/Molecules/RegisterCard';
-import { ChangeEvent, useEffect, useState } from 'react';
-import RegisterFormTemplate from '../../Components/Templates/RegisterFormTemplate';
-import { AdditionalItem, Item } from '../../Types/types';
-import Alert from '../../Components/Molecules/Alert';
-import AdditionalItemRepositories from '../../Services/repositories/AdditionalItemRepositories';
-import { LoadingFullScreenTemplate } from '../../Components/Templates/LoadingFullScreenTemplate';
-import ItemRepositories from '../../Services/repositories/ItemRepositories';
+import RegisterCard from "../../Components/Molecules/RegisterCard";
+import { ChangeEvent, useEffect, useState } from "react";
+import RegisterFormTemplate from "../../Components/Templates/RegisterFormTemplate";
+import { AdditionalItem, Item } from "../../Types/types";
+import Alert from "../../Components/Molecules/Alert";
+import AdditionalItemRepositories from "../../Services/repositories/AdditionalItemRepositories";
+import { LoadingFullScreenTemplate } from "../../Components/Templates/LoadingFullScreenTemplate";
+import ItemRepositories from "../../Services/repositories/ItemRepositories";
 
-const ERROR_NAME_MESSAGE = 'Nome inválido.';
-const ERROR_DESCRIPTION_MESSAGE = 'Descrição inválida.';
-const ERROR_PHOTO_MESSAGE = 'Foto inválida.';
-const ERROR_COD_ITEM_MESSAGE = 'Código do item inválido.';
-const ERROR_TYPE_MESSAGE = 'Tipo inválido.';
-const ERROR_VALUE_MESSAGE = 'Valor inválido.';
-const ERROR_QUANTITY_MESSAGE = 'Quantidade inválida.';
-const ERROR_COD_ITEM_ALREADY_EXIST = 'Já existe um item com esse código.'
+const ERROR_NAME_MESSAGE = "Nome inválido.";
+const ERROR_DESCRIPTION_MESSAGE = "Descrição inválida.";
+const ERROR_PHOTO_MESSAGE = "Foto inválida.";
+const ERROR_COD_ITEM_MESSAGE = "Código do item inválido.";
+const ERROR_TYPE_MESSAGE = "Tipo inválido.";
+const ERROR_VALUE_MESSAGE = "Valor inválido.";
+const ERROR_QUANTITY_MESSAGE = "Quantidade inválida.";
+const ERROR_COD_ITEM_ALREADY_EXIST = "Já existe um item com esse código.";
 
-const ALERT_MESSAGE_SUBITEM_CREATED = 'Subitem criado com sucesso.'
-const ALERT_MESSAGE_ITEM_CREATED = 'Item criado com sucesso.'
+const ALERT_MESSAGE_SUBITEM_CREATED = "Subitem criado com sucesso.";
+const ALERT_MESSAGE_ITEM_CREATED = "Item criado com sucesso.";
 
 export default function Register() {
-  const [isItemActive, setIsItemActive] = useState(false)
-  const [isSubItemActive, setIsSubItemActive] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [allItems, setAllItems] = useState<Item[]>()
+  const [isItemActive, setIsItemActive] = useState(false);
+  const [isSubItemActive, setIsSubItemActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [allItems, setAllItems] = useState<Item[]>();
   const [subitem, setSubitem] = useState<AdditionalItem>({
-    name: '',
-    description: '',
-    photo: ''
-  })
+    name: "",
+    description: "",
+    photo: "",
+  });
   const [subitemError, setSubitemError] = useState({
-    nameError: '',
-    descriptionError: '',
-    photoError: ''
-  })
+    nameError: "",
+    descriptionError: "",
+    photoError: "",
+  });
   const [item, setItem] = useState<Item>({
-    cod_item: '',
-    name: '',
-    type: '',
-    description: '',
+    cod_item: "",
+    name: "",
+    type: "",
+    description: "",
     value: 0,
     quantity: 0,
-    photo: '',
-    isVisible: false,
+    photo: "",
+    isVisible: true,
     additionals: [],
     additionals_sauces: [],
     additionals_drinks: [],
     additionals_sweets: [],
   });
   const [itemError, setItemError] = useState({
-    cod_itemError: '',
-    nameError: '',
-    typeError: '',
-    descriptionError: '',
-    valueError: '',
-    quantityError: '',
-    photoError: '',
+    cod_itemError: "",
+    nameError: "",
+    typeError: "",
+    descriptionError: "",
+    valueError: "",
+    quantityError: "",
+    photoError: "",
   });
-  const [alertInfo, setAlertInfo] = useState<{ isOpen: boolean, message: string, type: string }>({
+  const [alertInfo, setAlertInfo] = useState<{
+    isOpen: boolean;
+    message: string;
+    type: string;
+  }>({
     isOpen: false,
     message: "",
-    type: ""
+    type: "",
   });
 
   const showAlert = (message: string, type: string) => {
     setAlertInfo({
       isOpen: true,
       message: message,
-      type: type
+      type: type,
     });
   };
 
@@ -79,23 +83,22 @@ export default function Register() {
     setAlertInfo({
       isOpen: false,
       message: "",
-      type: ""
+      type: "",
     });
   };
 
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        const items = await ItemRepositories.getItems()
-        setAllItems(items)
+        const items = await ItemRepositories.getItems();
+        setAllItems(items);
       } catch (error) {
-        console.log("Não foi possível carregar os itens:", error)
+        console.log("Não foi possível carregar os itens:", error);
       }
-    }
+    };
 
-    fetchItems()
-
-  }, [])
+    fetchItems();
+  }, [setAllItems, setItem]);
 
   const validateSubitemForm = () => {
     let isValid = true;
@@ -105,7 +108,7 @@ export default function Register() {
       newSubitemError.nameError = ERROR_NAME_MESSAGE;
       isValid = false;
     } else {
-      newSubitemError.nameError = '';
+      newSubitemError.nameError = "";
     }
 
     // if (!subitem.description.trim()) {
@@ -134,14 +137,14 @@ export default function Register() {
       newItemError.nameError = ERROR_NAME_MESSAGE;
       isValid = false;
     } else {
-      newItemError.nameError = '';
+      newItemError.nameError = "";
     }
 
     if (!item.description.trim()) {
       newItemError.descriptionError = ERROR_DESCRIPTION_MESSAGE;
       isValid = false;
     } else {
-      newItemError.descriptionError = '';
+      newItemError.descriptionError = "";
     }
 
     // if (!item.photo.trim()) {
@@ -155,28 +158,28 @@ export default function Register() {
       newItemError.cod_itemError = ERROR_COD_ITEM_MESSAGE;
       isValid = false;
     } else {
-      newItemError.cod_itemError = '';
+      newItemError.cod_itemError = "";
     }
 
     if (!item.type.trim()) {
       newItemError.typeError = ERROR_TYPE_MESSAGE;
       isValid = false;
     } else {
-      newItemError.typeError = '';
+      newItemError.typeError = "";
     }
 
     if (item.value === 0) {
       newItemError.valueError = ERROR_VALUE_MESSAGE;
       isValid = false;
     } else {
-      newItemError.valueError = '';
+      newItemError.valueError = "";
     }
 
     if (item.quantity === 0) {
       newItemError.quantityError = ERROR_QUANTITY_MESSAGE;
       isValid = false;
     } else {
-      newItemError.quantityError = '';
+      newItemError.quantityError = "";
     }
 
     setItemError(newItemError);
@@ -185,216 +188,225 @@ export default function Register() {
 
   const handleSubmitSubitem = async () => {
     if (!validateSubitemForm()) {
-      console.log('Formulário Inválido.')
-      return
+      console.log("Formulário Inválido.");
+      return;
     }
 
-    setIsLoading(true)
+    setIsLoading(true);
 
     try {
       await AdditionalItemRepositories.createAdditionalItem(subitem);
-      setIsSubItemActive(false)
+      setIsSubItemActive(false);
       showAlert(ALERT_MESSAGE_SUBITEM_CREATED, "success");
-      setIsLoading(false)
+      setIsLoading(false);
       setSubitem({
-        name: '',
-        description: '',
-        photo: ''
-      })
+        name: "",
+        description: "",
+        photo: "",
+      });
       console.log("Subitem criado com sucesso");
     } catch (error) {
-      setIsLoading(false)
+      setIsLoading(false);
       console.error("Erro ao criar subitem:", error);
     }
-  }
+  };
 
   const handleSubmitItem = async () => {
     if (!validateItemForm()) {
-      console.log('Formulário Inválido.')
-      return
+      console.log("Formulário Inválido.");
+      return;
     }
 
-    if (allItems?.some((itemToFind: Item) => itemToFind.cod_item === item.cod_item)) {
-      setItemError({ ...itemError, cod_itemError: ERROR_COD_ITEM_ALREADY_EXIST })
-      return
+    if (
+      allItems?.some(
+        (itemToFind: Item) => itemToFind.cod_item === item.cod_item
+      )
+    ) {
+      setItemError({
+        ...itemError,
+        cod_itemError: ERROR_COD_ITEM_ALREADY_EXIST,
+      });
+      return;
     }
 
-    setIsLoading(true)
+    setIsLoading(true);
 
     try {
       await ItemRepositories.createItem(item);
-      setIsItemActive(false)
+      setIsItemActive(false);
       showAlert(ALERT_MESSAGE_ITEM_CREATED, "success");
-      setIsLoading(false)
+      setIsLoading(false);
       setItem({
-        cod_item: '',
-        name: '',
-        type: '',
-        description: '',
+        cod_item: "",
+        name: "",
+        type: "",
+        description: "",
         value: 0,
         quantity: 0,
-        photo: '',
-        isVisible: false,
+        photo: "",
+        isVisible: true,
         additionals: [],
         additionals_sauces: [],
         additionals_drinks: [],
         additionals_sweets: [],
-      })
+      });
       console.log("Item criado com sucesso");
     } catch (error) {
-      setIsLoading(false)
+      setIsLoading(false);
       console.error("Erro ao criar item:", error);
     }
-  }
+  };
 
   return (
     <section className={styles.registerContainer}>
-      <Text fontSize='extraLarge' fontWeight='semibold'>Cadastro de novos itens</Text>
+      <Text fontSize="extraLarge" fontWeight="semibold">
+        Cadastro de novos itens
+      </Text>
       <div className={styles.registerContent}>
         <div className={styles.cards}>
           <RegisterCard
-            label='Cadastrar Subitem'
-            description='Cadastre um subitem que poderá ser vinculado a um item em sua criação'
+            label="Cadastrar Subitem"
+            description="Cadastre um subitem que poderá ser vinculado a um item em sua criação"
             icon={<FontAwesomeIcon size="3x" icon={faWineBottle} />}
             onClick={() => {
-              setIsSubItemActive(!isSubItemActive)
-              setIsItemActive(false)
+              setIsSubItemActive(!isSubItemActive);
+              setIsItemActive(false);
             }}
             isActive={isSubItemActive}
           />
           <RegisterCard
-            label='Cadastrar Item'
-            description='Cadastre um item para aparecer no cardápio (o item já poderá ser visto pelos usuários)'
+            label="Cadastrar Item"
+            description="Cadastre um item para aparecer no cardápio (o item já poderá ser visto pelos usuários)"
             icon={<FontAwesomeIcon size="3x" icon={faBurger} />}
             onClick={() => {
-              setIsItemActive(!isItemActive)
-              setIsSubItemActive(false)
+              setIsItemActive(!isItemActive);
+              setIsSubItemActive(false);
             }}
             isActive={isItemActive}
           />
         </div>
         {isLoading && <LoadingFullScreenTemplate />}
-        {isSubItemActive &&
+        {isSubItemActive && (
           <RegisterFormTemplate
-            label='Cadastro de subitem'
+            label="Cadastro de subitem"
             inputs={[
               {
                 value: subitem.name,
-                placeholder: 'Nome',
+                placeholder: "Nome",
                 onChange: (e: ChangeEvent<HTMLInputElement>) => {
-                  setSubitem({ ...subitem, name: e.target.value })
-                  setSubitemError({ ...subitemError, nameError: '' });
+                  setSubitem({ ...subitem, name: e.target.value });
+                  setSubitemError({ ...subitemError, nameError: "" });
                 },
-                type: 'text',
-                errorLabel: subitemError.nameError
+                type: "text",
+                errorLabel: subitemError.nameError,
               },
               {
                 value: subitem.description,
-                placeholder: 'Descrição',
+                placeholder: "Descrição",
                 onChange: (e: ChangeEvent<HTMLInputElement>) => {
                   setSubitem({ ...subitem, description: e.target.value });
-                  setSubitemError({ ...subitemError, descriptionError: '' });
+                  setSubitemError({ ...subitemError, descriptionError: "" });
                 },
-                type: 'text',
-                errorLabel: subitemError.descriptionError
+                type: "text",
+                errorLabel: subitemError.descriptionError,
               },
               {
                 value: subitem.photo,
-                placeholder: 'Foto',
+                placeholder: "Foto",
                 onChange: (e: ChangeEvent<HTMLInputElement>) => {
                   setSubitem({ ...subitem, photo: e.target.value });
-                  setSubitemError({ ...subitemError, photoError: '' });
+                  setSubitemError({ ...subitemError, photoError: "" });
                 },
-                type: 'text',
-                errorLabel: subitemError.photoError
+                type: "text",
+                errorLabel: subitemError.photoError,
               },
             ]}
             buttonOnClick={handleSubmitSubitem}
-            buttonLabel='Enviar'
+            buttonLabel="Enviar"
           />
-        }
-        {isItemActive &&
+        )}
+        {isItemActive && (
           <RegisterFormTemplate
-            label='Cadastro de item'
+            label="Cadastro de item"
             inputs={[
               {
                 value: item.name,
-                placeholder: 'Nome',
+                placeholder: "Nome",
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                   setItem({ ...item, name: e.target.value });
-                  setItemError({ ...itemError, nameError: '' });
+                  setItemError({ ...itemError, nameError: "" });
                 },
-                type: 'text',
-                errorLabel: itemError.nameError
+                type: "text",
+                errorLabel: itemError.nameError,
               },
               {
                 value: item.description,
-                placeholder: 'Descrição',
+                placeholder: "Descrição",
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                   setItem({ ...item, description: e.target.value });
-                  setItemError({ ...itemError, descriptionError: '' });
+                  setItemError({ ...itemError, descriptionError: "" });
                 },
-                type: 'text',
-                errorLabel: itemError.descriptionError
+                type: "text",
+                errorLabel: itemError.descriptionError,
               },
               {
                 value: item.photo,
-                placeholder: 'Foto',
+                placeholder: "Foto",
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                   setItem({ ...item, photo: e.target.value });
-                  setItemError({ ...itemError, photoError: '' });
+                  setItemError({ ...itemError, photoError: "" });
                 },
-                type: 'text',
-                errorLabel: itemError.photoError
+                type: "text",
+                errorLabel: itemError.photoError,
               },
               {
                 value: item.cod_item,
-                placeholder: 'Código do Item',
+                placeholder: "Código do Item",
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                   setItem({ ...item, cod_item: e.target.value });
-                  setItemError({ ...itemError, cod_itemError: '' });
+                  setItemError({ ...itemError, cod_itemError: "" });
                 },
-                type: 'text',
-                errorLabel: itemError.cod_itemError
+                type: "text",
+                errorLabel: itemError.cod_itemError,
               },
               {
                 value: item.type,
-                placeholder: 'Tipo',
+                placeholder: "Tipo",
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                   setItem({ ...item, type: e.target.value });
-                  setItemError({ ...itemError, typeError: '' });
+                  setItemError({ ...itemError, typeError: "" });
                 },
-                type: 'text',
-                errorLabel: itemError.typeError
+                type: "text",
+                errorLabel: itemError.typeError,
               },
               {
-                value: item.value !== 0 ? item.value.toString() : '',
-                placeholder: 'Valor',
+                value: item.value !== 0 ? item.value.toString() : "",
+                placeholder: "Valor",
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                   setItem({ ...item, value: parseFloat(e.target.value) });
-                  setItemError({ ...itemError, valueError: '' });
+                  setItemError({ ...itemError, valueError: "" });
                 },
-                type: 'number',
-                errorLabel: itemError.valueError
+                type: "number",
+                errorLabel: itemError.valueError,
               },
               {
-                value: item.quantity !== 0 ? item.quantity.toString() : '',
-                placeholder: 'Quantidade',
+                value: item.quantity !== 0 ? item.quantity.toString() : "",
+                placeholder: "Quantidade",
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                   setItem({ ...item, quantity: parseInt(e.target.value) });
-                  setItemError({ ...itemError, quantityError: '' });
+                  setItemError({ ...itemError, quantityError: "" });
                 },
-                type: 'number',
-                errorLabel: itemError.quantityError
+                type: "number",
+                errorLabel: itemError.quantityError,
               },
             ]}
             withSubitem
             itemWithSubitems={item}
             setItemWithSubitems={setItem}
             buttonOnClick={handleSubmitItem}
-            buttonLabel='Enviar'
+            buttonLabel="Enviar"
           />
-        }
+        )}
       </div>
       <Alert
         isAlertOpen={alertInfo.isOpen}
@@ -405,5 +417,5 @@ export default function Register() {
         type={alertInfo.type}
       />
     </section>
-  )
+  );
 }

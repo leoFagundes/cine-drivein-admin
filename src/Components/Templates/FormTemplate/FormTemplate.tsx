253773dfbd24c5b5bd4401a89d0bdae661033616
leoFagundes <leofagundes2015@gmail.com>
2024-03-19 22:16:45 -0300

    update details

--- src/Components/Templates/FormTemplate/FormTemplate.tsx
+++ src/Components/Templates/FormTemplate/FormTemplate.tsx
@@ -1,14 +1,14 @@
-import styles from './FormTemplate.module.scss';
+import styles from "./FormTemplate.module.scss";
 import LogoImage from "../../Atoms/LogoImage";
 import Text from "../../Atoms/Text";
 import { Input } from "../../Atoms/Input/Input";
 import Button from "../../Atoms/Button";
-import { ChangeEvent, ReactNode, useState } from 'react';
-import Caption from '../../Molecules/Caption';
-import CheckBox from '../../Atoms/CheckBox';
+import { ChangeEvent, ReactNode, useState } from "react";
+import Caption from "../../Molecules/Caption";
+import CheckBox from "../../Atoms/CheckBox";
 
 type FormTemplateType = {
-  label: string,
+  label: string;
   inputs: Array<{
     value: string;
     placeholder: string;
@@ -29,31 +29,47 @@ type FormTemplateType = {
     type?: string;
     errorLabel: string;
     caption?: ReactNode;
-  }
-  logo?: boolean
-}
+  };
+  logo?: boolean;
+};
 
-export const FormTemplate = ({ label, inputs, buttonLabel, buttonOnClick, linkLabel, linkIcon, linkOnClick, createAccountTokenInfo, logo = true }: FormTemplateType) => {
+export const FormTemplate = ({
+  label,
+  inputs,
+  buttonLabel,
+  buttonOnClick,
+  linkLabel,
+  linkIcon,
+  linkOnClick,
+  createAccountTokenInfo,
+  logo = true,
+}: FormTemplateType) => {
   const [isChecked, setChecked] = useState(false);
   const getInputMargin = (index: number, errorLabel: string) => {
-    if (index === 0) return ''
-    if (errorLabel) return '12px'
-    return '24px'
-  }
+    if (index === 0) return "";
+    if (errorLabel) return "12px";
+    return "24px";
+  };
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       e.preventDefault();
       buttonOnClick();
     }
   };
+  const handleCheckboxChange = () => {
+    setChecked(!isChecked);
+  };
+
   return (
     <section className={styles.container}>
       {logo && <LogoImage marginBottom="40px" />}
-      <Text marginBottom="32px" fontSize='extraLarge'>{label}</Text>
+      <Text marginBottom="32px" fontSize="extraLarge">
+        {label}
+      </Text>
       <div className={styles.inputs}>
-        {
-          inputs.map((item, index) => {
-            return <Input
+        {inputs.map((item, index) => {
+          return (
+            <Input
               key={item.placeholder}
               value={item.value}
               placeholder={item.placeholder}
@@ -64,30 +80,47 @@ export const FormTemplate = ({ label, inputs, buttonLabel, buttonOnClick, linkLa
               caption={item.caption}
               onKeyDown={handleKeyPress}
             />
-          })
-        }
+          );
+        })}
       </div>
-      {createAccountTokenInfo && <div className={styles.adminContent}>
-        <Caption
-          marginTop="4px"
-          label={'Dar permissões de admin para este usuário?'}
-          checkboxRight={<CheckBox id='admin-checkbox' checked={isChecked} onChange={() => setChecked(!isChecked)} />}
-          fontSize='small'
-        />
-        {isChecked &&
-          <Input
-            value={createAccountTokenInfo.value}
-            onChange={createAccountTokenInfo.onChange}
-            placeholder={createAccountTokenInfo.placeholder}
-            errorLabel={createAccountTokenInfo.errorLabel}
-            marginTop='8px'
-            type={createAccountTokenInfo.type}
-            onKeyDown={handleKeyPress}
-          />
-        }
-      </div>}
+      {createAccountTokenInfo && (
+        <div className={styles.adminContent}>
+          <div onClick={handleCheckboxChange}>
+            <Caption
+              marginTop="4px"
+              label={"Dar permissões de admin para este usuário?"}
+              isLink
+              checkboxRight={
+                <CheckBox
+                  id="admin-checkbox"
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                />
+              }
+              fontSize="small"
+            />
+          </div>
+          {isChecked && (
+            <Input
+              value={createAccountTokenInfo.value}
+              onChange={createAccountTokenInfo.onChange}
+              placeholder={createAccountTokenInfo.placeholder}
+              errorLabel={createAccountTokenInfo.errorLabel}
+              marginTop="8px"
+              type={createAccountTokenInfo.type}
+              onKeyDown={handleKeyPress}
+            />
+          )}
+        </div>
+      )}
       <Button label={buttonLabel} onClick={buttonOnClick} />
-      <Caption marginTop="14px" onClick={linkOnClick} isLink iconLeft={linkIcon} label={linkLabel || ''} />
+      <Caption
+        marginTop="14px"
+        onClick={linkOnClick}
+        isLink
+        iconLeft={linkIcon}
+        label={linkLabel || ""}
+      />
     </section>
-  )
-}
+  );
+};

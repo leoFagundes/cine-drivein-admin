a6cb143b6a84843515c523cde4bcc72dc7d58466
leoFagundes <leofagundes2015@gmail.com>
2024-09-14 18:35:33 -0300

    update details

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   // baseURL: "http://localhost:3001",
   // baseURL: "https://cine-drivein-backend.onrender.com",
-  baseURL: "https://cine-drivein-backend-production.onrender.com",
+  baseURL: process.env.BACK_END_CONNECTION,
   headers: {
     Accept: "application/json, text/plain, */*",
     "Content-Type": "application/json",
